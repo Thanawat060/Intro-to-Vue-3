144e763d656b7b60bf02fa44c640744eb1b0d6b2
Thanawat060 <6206021620060@fitm.kmutnb.ac.th>
2021-03-24 16:22:19 +0700

    date

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ const app = Vue.createApp({
     data() {
         return {
             product: 'Socks',
-            image : './assets/images/socks_blue.jpg'
+            image : './assets/images/socks_green.jpg',
+            url : 'www.google.co.th'
         }
     }
 })
